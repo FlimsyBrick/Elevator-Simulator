e831905443efb82b9b834cd2dc7e6729a725bfc8
FlimsyBrick <141807842+FlimsyBrick@users.noreply.github.com>
2024-12-05 14:10:39 -0500

    Create ElevatorObserver.h

---
+++ ElevatorObserver.h
@@ -0,0 +1,26 @@
+#ifndef SimpleObserver_h
+#define SimpleObserver_h
+
+#include "ECObserver.h"
+#include "ECGraphicViewImp.h"
+
+
+//************************************************************
+class ECSimpleGraphicObserver : public ECObserver
+{
+public:
+    ECSimpleGraphicObserver( ECGraphicViewImp &viewIn );
+    virtual void Update();
+    
+private:
+    ECGraphicViewImp &view;
+    bool fIsSpaceBarPressed;
+    bool fIsUpKeyPressed;
+    bool fIsDownKeyPressed;
+    bool fIsLeftKeyPressed;
+    bool fIsRightKeyPressed;
+};
+
+bool is_point_in_triangle(int px, int py, int x1, int y1, int x2, int y2, int x3, int y3);
+
+#endif /* SimpleObserver_h */
