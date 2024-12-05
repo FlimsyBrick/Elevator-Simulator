02101836d24422ad9c35921addd626cea2aa4215
FlimsyBrick <141807842+FlimsyBrick@users.noreply.github.com>
2024-12-05 12:45:57 -0500

    Observer.h

---
+++ Observer.h
@@ -0,0 +1,40 @@
+#ifndef ECOBERVER_H
+#define ECOBERVER_H
+
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+class ECObserver
+{
+public:
+    virtual ~ECObserver() {}
+    virtual void Update() = 0;
+};
+
+class ECObserverSubject
+{
+public:
+    ECObserverSubject() {}
+    virtual ~ECObserverSubject() {}
+    void Attach( ECObserver *pObs ){
+    //std::cout << "Adding an observer.\n";
+        listObservers.push_back(pObs);
+    }
+    void Detach( ECObserver *pObs ){
+        listObservers.erase(std::remove(listObservers.begin(), listObservers.end(), pObs), listObservers.end());
+    }
+    void Notify()
+    {
+    //std::cout << "Notify: number of observer: " << listObservers.size() << std::endl;
+        for(unsigned int i=0; i<listObservers.size(); ++i)
+        {
+            listObservers[i]->Update();
+        }
+    }
+    
+private:
+    std::vector<ECObserver *> listObservers;
+};
+
+#endif
